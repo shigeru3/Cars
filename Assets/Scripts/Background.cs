f2f8bdd48879dca089820e0a1f29d8847610282d
shigeru <hironoki3@gmail.com>
2015-02-11 16:27:35 +0900

    background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+	public float speed = 0.1f;
+	
+	// Update is called once per frame
+	void Update () {
+		float y = Mathf.Repeat (Time.time * speed, 1);
+		Vector2 offset = new Vector2 (0, y);
+		renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+}
