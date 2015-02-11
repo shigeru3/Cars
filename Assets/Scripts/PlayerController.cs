1fdc614c6e199e71f449dbe4ea407714c4bb5e0d
shigeru <hironoki3@gmail.com>
2015-02-11 17:02:33 +0900

    player car

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 5;
+
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+		float y = Input.GetAxisRaw ("Vertical");
+		Vector2 direction = new Vector2 (x, y).normalized;
+		rigidbody2D.velocity = direction * speed;
+	}
+}
