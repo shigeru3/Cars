31080b8c0c0e8e45449dc01d60550df0e18da733
shigeru <hironoki3@gmail.com>
2015-03-23 11:22:05 +0900

    2d

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,20 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	public float speed = 5;
+	public float speedX = 5;
+	public float speedY = 1;
 
 	// Update is called once per frame
 	void Update () {
 		float x = Input.GetAxisRaw ("Horizontal");
+		Vector2 direction = new Vector2 (x, 0).normalized;
+		rigidbody2D.velocity = direction * speedX;
+
 		float y = Input.GetAxisRaw ("Vertical");
-		Vector2 direction = new Vector2 (x, y).normalized;
-		rigidbody2D.velocity = direction * speed;
+		if (y > 0) {
+			GameObject.Find ("Backgrounds").GetComponent<Speed> ().speed += 0.005f;
+		} else if (y < 0) {
+			GameObject.Find ("Backgrounds").GetComponent<Speed> ().speed -= 0.007f;
+		}
 	}
 }
---
+++ Assets/Scripts/Speed.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Speed: MonoBehaviour {
+		public GameObject Front;
+		public GameObject Back;
+		public GameObject Middle;
+
+		public float speed = 0.0f;
+		public float speedMax = 0.6f;
+		public float speedMin = 0.0f;
+
+		float position = 0.0f;
+
+		void Update() {
+				if (speed > speedMax) {
+						speed = speedMax;
+				} else if (speed < speedMin) {
+						speed = speedMin;
+				}
+				position += speed;
+				float frontY = Mathf.Repeat (position * 0.1f, 1);
+				float backY = Mathf.Repeat (position * 0.06f, 1);
+				float middleY = Mathf.Repeat (position * 0.08f, 1);
+				Front.renderer.sharedMaterial.SetTextureOffset ("_MainTex", new Vector2 (0, frontY));
+				Back.renderer.sharedMaterial.SetTextureOffset ("_MainTex", new Vector2 (0, backY));
+				Middle.renderer.sharedMaterial.SetTextureOffset ("_MainTex", new Vector2 (0, middleY));
+		}
+}
